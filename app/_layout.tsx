9ccb374a44c4358a8c825797fe12250a05fe2f33
Slavko Lukić <111462208+slavkolukic@users.noreply.github.com>
2025-09-21 00:07:50 +0200

    feat: install react-native-popup-menu

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,16 +2,17 @@ import '@/polyfills';
 
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { MenuProvider } from 'react-native-popup-menu';
 import 'react-native-reanimated';
 
 export default function RootLayout() {
   return (
-    <>
+    <MenuProvider>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </>
+    </MenuProvider>
   );
 }
