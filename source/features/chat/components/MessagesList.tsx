be6a78361b3259bcdaa726404afb1f4ae10cf3ee
Slavko Lukić <111462208+slavkolukic@users.noreply.github.com>
2025-09-21 13:53:44 +0200

    feat: implement loading

--- source/features/chat/components/MessagesList.tsx
+++ source/features/chat/components/MessagesList.tsx
@@ -1,10 +1,18 @@
+import { useTheme } from '@/source/core/hooks';
 import { UIMessage } from '@ai-sdk/react';
 import { FlashList, ListRenderItem } from '@shopify/flash-list';
-import { FC, useCallback } from 'react';
+import { FC, useCallback, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
 import { AssistantMessage } from './AssistantMessage';
 import { UserMessage } from './UserMessage';
-
 type Props = {
   messages: UIMessage[];
 };
@@ -37,11 +45,14 @@ export const MessagesList: FC<Props> = ({ messages }) => {
     []
   );
 
+  const lastMessageSentByUser = messages[messages.length - 1].role === 'user';
+
   return (
     <FlashList
       data={messages}
       renderItem={renderItem}
       ItemSeparatorComponent={ItemSeparatorComponent}
+      ListFooterComponent={lastMessageSentByUser ? MessageLoading : null}
       contentContainerStyle={styles.container}
       estimatedItemSize={95}
     />
@@ -55,8 +66,48 @@ const styles = StyleSheet.create({
   separator: {
     height: 12,
   },
+  loadingContainer: {
+    paddingHorizontal: 16,
+    marginTop: 18,
+  },
 });
 
 const ItemSeparatorComponent = () => {
   return <View style={styles.separator} />;
+};
+
+const MessageLoading = () => {
+  const { theme } = useTheme();
+  const opacity = useSharedValue(0);
+  const scale = useSharedValue(0);
+
+  useEffect(() => {
+    opacity.value = withTiming(1, { duration: 600 });
+    scale.value = withSequence(
+      withTiming(1, { duration: 600 }),
+      withRepeat(
+        withSequence(
+          withDelay(400, withTiming(0.6, { duration: 600 })),
+          withTiming(1, { duration: 600 })
+        ),
+        -1,
+        true
+      )
+    );
+  }, [opacity, scale]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    backgroundColor: theme.colors.backgroundContrast,
+    opacity: opacity.value,
+    transform: [{ scale: scale.value }],
+  }));
+
+  return (
+    <View style={styles.loadingContainer}>
+      <Animated.View style={animatedStyle} />
+    </View>
+  );
 };
