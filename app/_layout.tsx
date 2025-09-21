6c98c8ef75c178e77a9c011bdc7e057a2ce8bad9
Slavko Lukić <111462208+slavkolukic@users.noreply.github.com>
2025-09-21 22:24:39 +0200

    feat: change status bar color based on theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,21 @@
 import '@/polyfills';
 
+import { useTheme } from '@/source/core/hooks';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { MenuProvider } from 'react-native-popup-menu';
 import 'react-native-reanimated';
 
 export default function RootLayout() {
+  const { isDark } = useTheme();
+
   return (
     <MenuProvider>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar style={isDark ? 'light' : 'dark'} />
     </MenuProvider>
   );
 }
